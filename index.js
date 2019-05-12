c240548c307d811ac4bf2eb727ecce142a65275f
Chris Horn <chrishornuk@gmail.com>
2019-05-12 11:31:00 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,114 @@
+const EventEmitter = require('events')
+const amqp = require('amqplib' )
+const reconnectInterval = process.env.RMQ_RECONNECT_INTERVAL || 1000
+const rmqHost = process.env.RMQ_HOST || "amqp://localhost"
+
+let connection
+
+const start = async () => {
+
+  const e = new EventEmitter()
+
+  amqp.connect(rmqHost).then((conn) => {
+
+    console.log("connected!")
+    connection = conn
+
+    conn.on("error", (err) => {
+      if (err.message !== "Connection closing") {
+        console.error("[AMQP] conn error", err.message)
+        e.emit("error", err)
+      }
+    })
+
+    conn.on("close", () => {
+      console.error("[AMQP] reconnecting")
+      return setTimeout(start, reconnectInterval)
+    })
+
+    e.emit("connected")
+
+  }).catch((err) => {
+    e.emit("reconnecting")
+
+    if (err.message !== "Connection closing") {
+      console.error("[AMQP] catch:", err.message)
+      return setTimeout(start, reconnectInterval)
+    }
+  })
+  return e
+}
+
+const produce = async (queue, message, durable = false, persistent = false) => {
+  const channel = await connection.createChannel()
+
+  await channel.assertQueue(queue)
+  await channel.sendToQueue(queue, Buffer.from(message), { persistent })
+  console.log('Message produced: ', queue, message)
+
+}
+ 
+// Consumer
+const consume = async (queue, isNoAck = false, durable = false, prefetch = null) => {
+  const channel = await connection.createChannel()
+
+  await channel.assertQueue(queue)
+
+  if (prefetch) {
+    channel.prefetch(prefetch)
+  }
+  const consumeEmitter = new EventEmitter()
+  try {
+    channel.consume(queue, message => {
+      if (message !== null) {
+        consumeEmitter.emit('data', message.content.toString(), () => channel.ack(message))
+      } else {
+        const error = new Error('NullMessageException')
+        consumeEmitter.emit('error', error)
+      }
+    }, {noAck: isNoAck})
+  } catch (error) {
+    consumeEmitter.emit('error', error)
+  }
+  return consumeEmitter
+}
+
+const publish = async (exchangeName, exchangeType, message) => {
+  const channel = await connection.createChannel()
+
+  await channel.assertExchange(exchangeName, exchangeType, {durable: false})
+  await channel.publish(exchangeName, '', Buffer.from(message))
+
+  console.log('Message published: ', exchangeName, message)
+}
+
+const subscribe = async (exchangeName, exchangeType) => {
+  const channel = await connection.createChannel()
+
+  await channel.assertExchange(exchangeName, exchangeType, {durable: false})
+  const queue = await channel.assertQueue('', {exclusive: true})
+  channel.bindQueue(queue.queue, exchangeName, '')
+  const consumeEmitter = new EventEmitter()
+
+  try {
+    channel.consume(queue.queue, message => {
+      if (message !== null) {
+        consumeEmitter.emit('data', message.content.toString())
+      } else {
+        const error = new Error('NullMessageException')
+        consumeEmitter.emit('error', error)
+      }
+    }, {noAck: true})
+  } catch (error) {
+    consumeEmitter.emit('error', error)
+  }
+  return consumeEmitter
+}
+
+module.exports = {
+  start,
+  produce,
+  publish,
+  subscribe,
+  consume
+}
